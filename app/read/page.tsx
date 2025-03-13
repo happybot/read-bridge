74310370d7cf4e6146c656955b6357257c8c68e7
Echo <chroom@foxmail.com>
2025-03-13 16:11:31 +0800

    refactor: simplify useBook hook and update ReadPage to manage readingId

--- app/read/page.tsx
+++ app/read/page.tsx
@@ -1,13 +1,30 @@
 'use client'
 import { useBook } from "@/app/hooks/useBook"
-
+import { useSiderStore } from "@/store/useSiderStore"
+import { useEffect } from "react"
 export default function ReadPage({ searchParams }: { searchParams: { id: string } }) {
   const id = searchParams.id ?? ''
-  const book = useBook(id)
+  const { setReadingId } = useSiderStore()
+  useEffect(() => {
+    if (id) setReadingId(id)
+  }, [id, setReadingId])
+  const book = useBook()
 
+  if (!book) return <div>书籍不存在</div>
   return (
     <div className="w-full h-full p-2">
-      正在阅读书籍 ID: {id}, 正在阅读书籍 title: {book?.title}
+      正在阅读书籍 ID: {id}, 正在阅读书籍 title: {book.title}
+      {book.chapterList.map((chapter) => (
+        <div key={chapter.title}>
+          {chapter.lines.map(line => {
+            return (
+              <div key={line}>
+                {line}
+              </div>
+            )
+          })}
+        </div>
+      ))}
     </div>
   )
 } 
