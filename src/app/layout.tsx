6b17ef09bf2ce36cbdb24440e455a6bf19ed045b
Echo <chroom@foxmail.com>
2025-03-27 09:38:01 +0800

    refactor: remove unused local fonts and simplify layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+
 import "./globals.css";
 import { ThemeProvider } from "@/src/components/layout/theme-provider";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import StructureLayout from '@/src/components/layout/structure-layout';
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "ReadBridge",
@@ -32,7 +21,7 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes" />
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}>
+      <body className={`antialiased h-full`}>
         <ThemeProvider>
           <AntdRegistry>
             <StructureLayout>
