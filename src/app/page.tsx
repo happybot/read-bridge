a4d8ffd7722cbb60cf096ed9686a3568ad29c8fb
Echo <chroom@foxmail.com>
2025-03-27 15:20:44 +0800

    feat: cloudflare config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 "use client"
 
+// Cloudflare needs it, see: https://nextjs.org/docs/app/building-your-application/rendering/edge-and-nodejs-runtimes
+export const runtime = "nodejs"
+export const dynamic = "force-static"
+export const dynamicParams = false
+
+export const revalidate = 60
+
 import HomeContent from '@/src/app/home/page';
 
 export default function Home() {
