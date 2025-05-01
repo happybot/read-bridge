390442f1357b7b4619e7634ac4ef2ed49cfa29fc
Echo <chroom@foxmail.com>
2025-05-01 10:35:33 +0800

    feat:  add Tauri CLI support

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const isProd = process.env.NODE_ENV === 'production';
+
+const internalHost = process.env.TAURI_DEV_HOST || 'localhost';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
@@ -8,7 +12,12 @@ const nextConfig = {
       skipDefaultConversion: true,
     },
   },
-  // output: 'export', // 输出纯静态
+  output: 'export', // 输出纯静态
+  images: {
+    unoptimized: true,
+  },
+  // 配置 assetPrefix，否则服务器无法正确解析您的资产。
+  assetPrefix: isProd ? undefined : `http://${internalHost}:3000`,
   webpack: (config) => {
     return config;
   },
